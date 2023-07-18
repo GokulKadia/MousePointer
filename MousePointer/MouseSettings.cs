94bce40e24977def346d6a19f5709ecb22dd3730
GokulKadia <gokul.kadia@dellteam.com>
2023-07-18 07:14:17 +0100

    Mouse and Keyboard

---
+++ MousePointer/MouseSettings.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MousePointer
+{
+    public class MouseSettings
+    {
+        /// <summary>
+        /// Main Function is SystemparamtersInfo so i added in entrypoint and given custom name for calling and understanding
+        /// </summary>
+       [DllImport("user32.dll", SetLastError = true,EntryPoint = "SystemParametersInfo")]
+        static extern bool ApplyKBMouseSetting(int uiAction, int uiParam, int pvParam, int fWinIni);
+
+        /// <summary>
+        /// written this function for handling Cursor Blinking rate in Keyboard
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetCaretBlinkTime(int uMSeconds);
+
+        /// <summary>
+        /// written this function for handling the installation of the Keyboard langauge pack 
+        /// </summary>        
+        [DllImport("Input.dll", SetLastError = true,EntryPoint = "InstallLayoutOrTip")]
+        public static extern bool InstallLanguages([MarshalAsAttribute(UnmanagedType.LPWStr)] string lang, int flag);
+
+        /// <summary>
+        /// written this function for handling the Setting the Default Keyboard langauge pack 
+        /// </summary>        
+        [DllImport("Input.dll", SetLastError = true, EntryPoint = "SetDefaultLayoutOrTip")]
+        public static extern bool SetDefaultKBLanguages([MarshalAsAttribute(UnmanagedType.LPWStr)] string lang, int flag);
+
+        /// <summary>
+        /// This is for fetching language name from Code
+        /// </summary>        
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern int LCIDToLocaleName(uint Locale,StringBuilder lpName, int cchName, int dwFlags);
+
+       
+        public enum KBMouseSet : int
+        {
+            //Mouse Settings
+            SPI_SETMOUSEBUTTONSWAP= 0x0021,
+            SPIF_UPDATEINIFILE= 0x0001,
+            SPI_SETDOUBLECLICKTIME= 0x0020,
+            SPI_SETMOUSETRAILS = 0x005D,
+            SPI_SETMOUSECLICKLOCK= 0x101F,
+            SPI_SETMOUSECLICKLOCKTIME= 0x2009,
+            SPI_SETMOUSEVANISH= 0x1021,
+            SPI_SETMOUSESONAR= 0x101D,
+            SPI_SETSNAPTODEFBUTTON = 0x0060,
+            SPI_SETWHEELSCROLLLINES= 0x0069,
+            SPI_SETMOUSESPEED= 0x0071,
+            //Keyboard Settings 
+            SPI_SETKEYBOARDCUES = 0x100B,
+            SPI_SETKEYBOARDDELAY= 0x0017,
+            SPI_SETKEYBOARDPREF = 0x0045,
+            SPI_SETKEYBOARDSPEED = 0x000B,
+            //Keyboard Language Handler
+            ILOT_CLEANINSTALL = 0x00000040,
+            ILOT_DEFPROFILE = 0x00000002,
+            ILOT_UNINSTALL = 0x00000001
+        }
+
+        public static bool SetKBMousePoiter(string MouseOption,int Value)
+        {
+            bool isResult = false;
+            switch (MouseOption)
+            {
+                case "MouseSwape":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETMOUSEBUTTONSWAP, Value,0, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "MouseDoubleClickTime":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETDOUBLECLICKTIME, Value, 0, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "MousePointerTrail":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETMOUSETRAILS, Value, 0, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "MouseLock":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETMOUSECLICKLOCK, 0, Value, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETMOUSECLICKLOCKTIME, 0, Value, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "MouseHide":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETMOUSEVANISH, 0, Value, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "MouseFind":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETMOUSESONAR, 0, Value, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "MouseSnapButton":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETSNAPTODEFBUTTON, Value, 0, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "MouseScroll":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETWHEELSCROLLLINES, Value, 0, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "MouseSpeed":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETMOUSESPEED, 0, Value, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "KBMenuAccess":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETKEYBOARDCUES, 0, Value, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "KBrepeatDelay":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETKEYBOARDDELAY, Value,0, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "KBpref":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETKEYBOARDPREF, Value, 0, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "KBRepeateRate":
+                    ApplyKBMouseSetting((int)KBMouseSet.SPI_SETKEYBOARDSPEED, Value, 0, (int)KBMouseSet.SPIF_UPDATEINIFILE);
+                    break;
+                case "KBBlinkrate":
+                    SetCaretBlinkTime(Value);
+                    break;               
+                default:
+                    break;
+            }
+            return isResult;
+        }
+
+        public static bool ApplyLanguageSettings(string langCode,string strinstall)
+        {
+            bool iSuccess = false;
+            //1)  Call Null Check
+            //2)  Install the Language Pack
+            iSuccess=strinstall=="Install"? InstallLanguages(langCode, (int)KBMouseSet.ILOT_CLEANINSTALL) : InstallLanguages(langCode, (int)KBMouseSet.ILOT_UNINSTALL);
+            return iSuccess;
+        }
+        public static bool ApplyDefaultDisplaylanguage(string langCode)
+        {
+            bool iSuccess = false;
+            //1)  Call Null Check
+            //2)  Install the Language Pack
+            iSuccess = InstallLanguages(langCode, (int)KBMouseSet.ILOT_DEFPROFILE);
+            return iSuccess;
+        }
+
+        public static string KB_CPL_INTER_REG_PATH = "Control Panel\\International";
+        public static string KB_CPL_DESK_REG_PATH = "Control Panel\\Desktop";
+
+        /// <summary>
+        /// this function is for checking defaultDisplayLanguage is already there or not if same is there then it retunr true other wise false 
+        /// </summary>
+        /// <param name="DispLang"></param>
+        /// <returns></returns>
+        public static bool CompareCurrentDefaultMethod(string DispLang)
+        {
+            bool isdefDispLang = false;
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(KB_CPL_INTER_REG_PATH);
+            string regValue= key.GetValue("Locale").ToString();
+            return isdefDispLang = regValue == DispLang ? true : false;
+        }
+    }
+}
---
+++ MousePointer/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MousePointer
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            #region Mouse Settings
+            // ApplyMouseSwapButton 
+            //MouseSettings.SetKBMousePoiter("MouseSwape",0); // This is for settings the MouseSwape  (Left-handed Mouse)
+            //MouseSettings.SetKBMousePoiter("MouseDoubleClickTime", 300); // This is for settings the MouseDoubleSpeed (Double Click Speed)
+            //MouseSettings.SetKBMousePoiter("MousePointerTrail", 1); // This is for settings the MouseTrails (Pointer Trail Length)
+            //MouseSettings.SetKBMousePoiter("MouseLock", 1); // This is for settings the MouseLock (Click Lock)
+            //MouseSettings.SetKBMousePoiter("MouseHide", 1); // This is for settings the Hide Mouse Pointer while typing (Hide Mouse Pointer)
+            //MouseSettings.SetKBMousePoiter("MouseFind", 1); // This is for settings the Show Mouse pointer Localtion while pressing Ctrl (Find Mouse Pointer)
+            //MouseSettings.SetKBMousePoiter("MouseSnapButton", 1); // This is for settings the Autometically move mouse pointer to default button dialogue box  (Snap Mouse Pointer)
+            //MouseSettings.SetKBMousePoiter("MouseSpeed", 10); // This is for settings the MouseSpeed (Mouse Speed)
+            //MouseSettings.SetKBMousePoiter("MouseScroll", 5); // This is for settings the MouseScroll (Scroll Lines)
+            #endregion
+
+            #region Keyboard
+            //MouseSettings.SetKBMousePoiter("KBMenuAccess", 1); // This is for Menu Access
+            //MouseSettings.SetKBMousePoiter("KBrepeatDelay", 5); // This is for KBrepeatDelay (Control Keyboard in Win+R)
+            //MouseSettings.SetKBMousePoiter("KBpref", 1); // This is for KBpref
+            //MouseSettings.SetKBMousePoiter("KBRepeateRate", 20); // This is for RepeateRate (Control Keyboard in Win+R)
+            //MouseSettings.SetKBMousePoiter("KBBlinkrate", 500); // This is for KBBlinkrate (Control Keyboard in Win+R)
+
+
+            /* 
+             Installing Language 
+            1) First fetch the distinct lang code from Payload 
+            2) Use Install function for installing Language Pack - PASSING FLAG FOR HANDLING INSTALL AND UNINSTALL
+            3) before installing display lang we check if it is already there or not if install lang is proper installed then Configure Display language for current Profile
+            4) after that we fetch the Name from Code of the Language for setting the registry
+            5) last step set the registry values
+            */
+
+            StringBuilder strLangName = new StringBuilder(85);
+            // int ss= MouseSettings.LCIDToLocaleName(0x0C0A, str,85,0); // Here in Argument we have to pass Lang code which is first in the Payload like 0409:00000409 so we need to pass 0409
+
+            string KB_CPL_DESK_REG_PATH = "Control Panel\\Desktop";
+            string KB_CPL_INTER_REG_PATH = "Control Panel\\International";
+            string KB_CPL_INTER_USR_PROFILE_REG_PATH = "Control Panel\\International\\User Profile";
+
+            if (MouseSettings.ApplyLanguageSettings("0416:00000416;0409:00000409;0C0A:0001040A;0409:00011009;0409:00004009;0414:0001043B;0414:00011809;", "UnInstall"))
+            {
+                if (MouseSettings.CompareCurrentDefaultMethod("0416:00000416") == false)
+                {
+                    if (MouseSettings.ApplyDefaultDisplaylanguage("0416:00000416;"))
+                    {
+                        if (MouseSettings.LCIDToLocaleName(0x0416, strLangName, 85, 0) != 0) // Here in Argument we have to pass Lang code which is first in the Payload like 0409:00000409 so we need to pass 0409
+                        {
+                            //Set registry 
+                            RegistryKey key = Registry.CurrentUser.OpenSubKey(KB_CPL_DESK_REG_PATH);
+                            key.SetValue("PreferredUILanguages", strLangName.ToString(),RegistryValueKind.MultiString);
+                            key.SetValue("PreferredUILanguagesPending", strLangName.ToString(), RegistryValueKind.MultiString);
+                            key = Registry.CurrentUser.OpenSubKey(KB_CPL_INTER_REG_PATH);
+                            key.SetValue("Locale", "00000416");
+                            key.SetValue("LocaleName", strLangName.ToString());
+                        }
+                    }
+                }
+            }
+
+            ////Configure Default Keyboard Layout
+            //if (MouseSettings.SetDefaultKBLanguages("0409:00004009;", 1))
+            //{
+            //    //Set tht Registry Path 
+            //    //KB_CPL_INTER_USR_PROFILE_REG_PATH with key- InputMethodOverride and Value -  "0409:00004009;" (Default Keyboard Layout)
+            //}
+
+
+            // Only Now MSGINA keyboard layou we need to check  if not req then no need
+
+            #endregion
+        }
+    }
+}
